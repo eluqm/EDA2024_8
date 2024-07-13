c33cc68ef6dad007f9bb68515d83ac191ab1a3ad
Yordano Boza Portilla <ybozap@unsa.edu.pe>
2024-07-13 13:05:32 -0500

    Agregando gitignore para evitar subir archivos innecesarios y modificando la clase Song. En esta clase se aumento mas atributos segun el encabezado del archivo csv, 2 getters y una salida de cadena(String) para cada cancion.

--- song.java
+++ song.java
@@ -1,36 +1,80 @@
-class Song{
-    int popularity;
-    int year;
-    double danceability;
-    double energy;
-    int key;
-    double loudness;
-    int mode;
-    double speechiness;
-    double acousticness;
-    double instrumentalness;
-    double liveness;
-    double valance;
-    double tempo;
-    int time_signature;
-    int duration_ms;
+public class Song {
+    String number;
+    String artistName; // Artista
+    String trackName; // Nombre de la canción
+    String trackId; // ID de la canción
+    String popularity; // Popularidad
+    String year; // Año
+    String genre; // Género
+    String danceability; // Danceability
+    String energy; // Energía
+    String key; // Clave
+    String loudness; // Sonoridad
+    String mode; // Modo
+    String speechiness; // Habladuría
+    String acousticness; // Acústica
+    String instrumentalness; // Instrumentalidad
+    String liveness; // Vivacidad
+    String valence; // Valencia
+    String tempo; // Tempo
+    String durationMs; // Duración en milisegundos
+    String timeSignature; // Firma de tiempo
 
-    public Song( int popularity, int year, double danceability, double energy, int key, double loudness, int mode, double speechiness, double acousticness, double instrumentalness,
-                 double liveness, double valance, double tempo, int time_signature, int duration_ms) {
+    public Song(String number, String artistName, String trackName, String trackId, String popularity, String year,
+            String genre, String danceability, String energy, String key, String loudness,
+            String mode, String speechiness, String acousticness, String instrumentalness,
+            String liveness, String valence, String tempo, String durationMs, String timeSignature) {
+        this.number = number;
+        this.artistName = artistName;
+        this.trackName = trackName;
+        this.trackId = trackId;
         this.popularity = popularity;
         this.year = year;
+        this.genre = genre;
         this.danceability = danceability;
         this.energy = energy;
         this.key = key;
-        this.loudness = loudness; 
+        this.loudness = loudness;
         this.mode = mode;
         this.speechiness = speechiness;
         this.acousticness = acousticness;
         this.instrumentalness = instrumentalness;
         this.liveness = liveness;
-        this.valance = valance;
+        this.valence = valence;
         this.tempo = tempo;
-        this.time_signature = time_signature;
-        this.duration_ms = duration_ms;             
+        this.durationMs = durationMs;
+        this.timeSignature = timeSignature;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    @Override
+    public String toString() {
+        return number + "," +
+                artistName + "," +
+                trackName + "," +
+                trackId + "," +
+                popularity + "," +
+                year + "," +
+                genre + "," +
+                danceability + "," +
+                energy + "," +
+                key + "," +
+                loudness + "," +
+                mode + "," +
+                speechiness + "," +
+                acousticness + "," +
+                instrumentalness + "," +
+                liveness + "," +
+                valence + "," +
+                tempo + "," +
+                durationMs + "," +
+                timeSignature;
     }
 }
