45fc50b97f713a969430906654232f2cdf153043
Yordano Boza Portilla <ybozap@unsa.edu.pe>
2024-07-13 13:30:32 -0500

    Se modifico la clase Node para poder trabajar con una lista doblemente enlazada, que posteriormente se creara.

--- Node.java
+++ Node.java
@@ -1,30 +1,40 @@
 public class Node<T> {
-    private T data;
-    private Node<T> next;
-    
-    public Node(T data, Node<T> next){
-      this.data = data;
-      this.next = next;
-    }
-    
-    public T getData(){
-      return data;
-    }
-    
-    public void setData(T data){
-      this.data = data;
-    }
-    
-    public Node<T> getNext(){
-      return next;
-    }
-    
-    public void setNext(Node<T> next){
-      this.next = next;
-    }
-    
-    @Override
-    public String toString(){
-      return this.data.toString();
-    }
+  private T data;
+  private Node<T> next;
+  private Node<T> prev;
+
+  public Node(T data, Node<T> next, Node<T> prev) {
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+
+  public T getData() {
+    return data;
+  }
+
+  public void setData(T data) {
+    this.data = data;
+  }
+
+  public Node<T> getNext() {
+    return next;
+  }
+
+  public void setNext(Node<T> next) {
+    this.next = next;
+  }
+
+  public Node<T> getPrev() {
+    return prev;
+  }
+
+  public void setPrev(Node<T> prev) {
+    this.prev = prev;
+  }
+
+  @Override
+  public String toString() {
+    return this.data.toString();
+  }
 }
