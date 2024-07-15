ff38dcec1d9cf538f38eb45e56a635f24cef26c6
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-15 14:15:32 -0500

    En el Main se hizo una interfaz grafica simple que aparece en consola donde primero se carga las canciones en SongManager y muestra la cantidad de canciones almacenadas.  Despues se crea una list de playlist para poder agregar mas de una playlist si el usuario lo requiere, y se muestra por decir asi el menu principal con opciones a elegir. Una vez creada la primera playlist llama al metodo managePlaylist que sera explicado posteriormente.

--- Main.java
+++ Main.java
@@ -1,25 +1,86 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-class Main{
-    public static void main(String[] args){
-        String csvFile = "spotify_data.csv";
-        try{
-            File archivo = new File(csvFile);
-            Scanner sc = new Scanner(archivo);
-            int fila = 0;
-            while (sc.hasNextLine() && fila < 50){
-                String siguiente = sc.nextLine();
-                String[] values = siguiente.split(",");
-                for (String value : values){
-                    System.out.print(value + " ");
-                }
-                System.out.println();
-                fila++;
+public class Main {
+    public static void main(String[] args) {
+        SongManager songManager = new SongManager();
+        songManager.loadSongsFromCSV("spotify_data.csv");
+        System.out.println(songManager.getSize());
+
+        List<Playlist> playlists = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Opciones:");
+            System.out.println("1. Crear una nueva playlist");
+            System.out.println("2. Agregar canciones a una playlist existente");
+            System.out.println("3. Eliminar canciones a una playlist existente");
+            System.out.println("4. Mostrar todas las playlists");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opcion: ");
+            int option = Integer.parseInt(scanner.nextLine());
+
+            switch (option) {
+                case 1:
+                    System.out.print("Ingrese el nombre de la nueva playlist: ");
+                    String playlistName = scanner.nextLine();
+                    Playlist newPlaylist = new Playlist(playlistName);
+                    playlists.add(newPlaylist);
+                    managePlaylist(songManager, newPlaylist, scanner);
+                    break;
+                case 2:
+                    if (playlists.isEmpty()) {
+                        System.out.println("No hay playlists creadas.");
+                        break;
+                    }
+                    System.out.println("Seleccione la playlist a la cual desea agregar canciones:");
+                    for (int i = 0; i < playlists.size(); i++) {
+                        System.out.println((i + 1) + ". " + playlists.get(i).getName());
+                    }
+                    int playlistChoice = Integer.parseInt(scanner.nextLine());
+                    if (playlistChoice < 1 || playlistChoice > playlists.size()) {
+                        System.out.println("Seleccion invalida.");
+                        break;
+                    }
+                    Playlist selectedPlaylist = playlists.get(playlistChoice - 1);
+                    managePlaylist(songManager, selectedPlaylist, scanner);
+                    break;
+                case 3:
+                    if (playlists.isEmpty()) {
+                        System.out.println("No hay playlists creadas.");
+                        break;
+                    }
+                    System.out.println("Seleccione la playlist a la cual desea eliminar canciones:");
+                    for (int i = 0; i < playlists.size(); i++) {
+                        System.out.println((i + 1) + ". " + playlists.get(i).getName());
+                    }
+                    playlistChoice = Integer.parseInt(scanner.nextLine());
+                    if (playlistChoice < 1 || playlistChoice > playlists.size()) {
+                        System.out.println("Seleccion invalida.");
+                        break;
+                    }
+                    selectedPlaylist = playlists.get(playlistChoice - 1);
+                    managePlaylist(songManager, selectedPlaylist, scanner);
+                    break;
+
+                case 4:
+                    if (playlists.isEmpty()) {
+                        System.out.println("No hay playlists creadas.");
+                    } else {
+                        System.out.println("Listas de reproduccion:");
+                        for (Playlist playlist : playlists) {
+                            System.out.println("- " + playlist.getName());
+                        }
+                    }
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    return;
+                default:
+                    System.out.println("Opcion invalida, por favor intente de nuevo.");
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
     }
+
 }
