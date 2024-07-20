b66edc0f9d9cf716538f9c455197d8a809a3a988
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-20 12:23:00 -0500

    Implementando el método splitChild para dividir un nodo en un árbol B y redistribuir las claves y los hijos

--- BTree.java
+++ BTree.java
@@ -9,4 +9,20 @@ public class BTree {
         this.t = t;
         this.comparator = comparator;
     }
+
+    private void splitChild(BTreeNode x, int i) {
+        BTreeNode y = x.children.get(i);
+        BTreeNode z = new BTreeNode(y.isLeaf);
+        for (int j = 0; j < t - 1; j++) {
+            z.keys.add(y.keys.remove(t));
+        }
+        if (!y.isLeaf) {
+            for (int j = 0; j < t; j++) {
+                z.children.add(y.children.remove(t));
+            }
+        }
+        x.children.add(i + 1, z);
+        x.keys.add(i, y.keys.remove(t - 1));
+    }
+   
 }
