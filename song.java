f2e4b0eec435a085c9c3669952afc7ae16886720
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-20 13:42:00 -0500

    Implementando los métodos getter y setter para los atributos de la clase Song

--- song.java
+++ song.java
@@ -14,4 +14,44 @@ public class Song {
         this.popularity = p;
         this.year = y;
     }
+	 public String getId_track() {
+        return id_track;
+    }
+
+    public void setId_track(String id_track) {
+        this.id_track = id_track;
+    }
+
+    public String getName_track() {
+        return name_track;
+    }
+
+    public void setName_track(String name_track) {
+        this.name_track = name_track;
+    }
+
+    public String getArtist_track() {
+        return artist_track;
+    }
+
+    public void setArtist_track(String artist_track) {
+        this.artist_track = artist_track;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    
 }
