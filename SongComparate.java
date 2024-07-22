707d9df090c1c71b4dba38d10a9f818537e4a42a
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-07-22 09:23:06 -0500

    Agregando metodo comparator, que ordena por popularidad las canciones

--- SongComparate.java
+++ SongComparate.java
@@ -15,4 +15,15 @@ public class SongComparate {
     public static Comparator<Song> byName(){
         return Comparator.comparing(Song::getName_track);
     }
+    public static Comparator<Song> byPopularity(){
+        return (s1, s2) -> {
+            try {
+                int pop1 = Integer.parseInt(s1.getPopularity());
+                int pop2 = Integer.parseInt(s2.getPopularity());
+                return Integer.compare(pop2, pop1);
+            } catch (NumberFormatException e) {
+                return s2.getPopularity().compareTo(s1.getPopularity());
+            }
+        };
+    }
 }
