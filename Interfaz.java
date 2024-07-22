474e3c3f1ae1f0a5fa7de2b4db41b0cfa5e2b498
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-07-22 09:36:01 -0500

    Se añadio escene, para visualizar la interfaz, además de agregar evento de dashboard, transladables, y estilos

--- Interfaz.java
+++ Interfaz.java
@@ -184,6 +184,132 @@ public class Interfaz extends Application {
             }
         });
 
+        TableColumn<Song, Void> updateColumn = new TableColumn<>("Update");
+        updateColumn.setCellFactory(param -> new TableCell<Song, Void>() {
+            private final Button updateButton = new Button();
+
+            {
+                updateButton.setGraphic(new ImageView(updateIcon));
+                updateButton.setOnAction(event -> {
+                    Song song = getTableView().getItems().get(getIndex());
+                    showUpdateMusicDialog((Stage) getTableView().getScene().getWindow(), music, song);
+                    orderingSearch(table, songs, comboBox.getValue());
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(updateButton);
+                }
+            }
+        });
+
+        table.getColumns().addAll(nameColumn, artistColumn, popularityColumn, yearColumn, idColumn, deleteColumn, updateColumn);
+        updateTable(table, songs);
+        table.setId("table");
+        table.setRowFactory(tv -> {
+            TableRow<Song> row = new TableRow<>();
+
+            row.setOnDragDetected(event -> {
+                if (!row.isEmpty()) {
+                    Dragboard db = row.startDragAndDrop(TransferMode.MOVE);
+                    ClipboardContent content = new ClipboardContent();
+                    content.putString(row.getItem().getName_track());
+                    db.setContent(content);
+                    event.consume();
+                }
+            });
+
+            row.setOnDragOver(event -> {
+                if (event.getGestureSource() != row && event.getDragboard().hasString()) {
+                    event.acceptTransferModes(TransferMode.MOVE);
+                }
+                event.consume();
+            });
+
+            row.setOnDragDropped(event -> {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasString()) {
+                    Song draggedSong = findSongByName(db.getString(), music);
+                    if (draggedSong != null) {
+                        table.getItems().remove(draggedSong);
+                        int index = row.getIndex();
+                        if (index >= 0) {
+                            table.getItems().add(index, draggedSong);
+                        } else {
+                            table.getItems().add(draggedSong); // Add to the end if index is invalid
+                        }
+                        success = true;
+                        updateTable(table, convertToDoublyLinkedList(table.getItems())); // Ensure styles are updated
+                    }
+                }
+                event.setDropCompleted(success);
+                event.consume();
+            });
+
+            row.setOnDragDone(event -> {
+                if (event.getTransferMode() == TransferMode.MOVE) {
+                    // Maneja lo que ocurre después de que el arrastre se completa
+                }
+                event.consume();
+            });
+
+            row.setOnMouseEntered(event -> {
+                if (row.isEmpty()) {
+                    row.setStyle("");
+                } else {
+                    row.setStyle("-fx-background-color: lightblue; -fx-text-fill: #000000;"); // Apply any style you want to keep during the drag
+                }
+            });
+
+            row.setOnMouseExited(event -> {
+                if (row.isEmpty()) {
+                    row.setStyle("");
+                }
+            });
+
+            return row;
+        });
+        HBox buttonBox = new HBox();
+        buttonBox.getStyleClass().add("button-container"); // Aplica el estilo CSS
+        buttonBox.getChildren().addAll(textField,searchButton, addMusicButton);
+        comboBox.getStyleClass().add("combo-box");
+        // Crear el VBox para el resto de los componentes
+        VBox root = new VBox();
+        root.getChildren().addAll(buttonBox, comboBox, table);
+        
+        searchButton.getStyleClass().add("button");
+        addMusicButton.getStyleClass().add("button");
+
+        // Aplica la clase CSS al TableView y sus columnasDeath Waiver
+        table.getStyleClass().add("table-view");
+
+        // Aplica la clase CSS a los contenedores
+        buttonBox.getStyleClass().add("hbox");
+        root.getStyleClass().add("vbox");
+        searchButton.getStyleClass().add("search-button"); // Aplica el estilo CSS
+
+        addMusicButton.getStyleClass().add("add-button"); 
+        // Aplica la clase CSS a los elementos de imagen
+        searchButton.setGraphic(new ImageView(searchIcon) {{
+            getStyleClass().add("image-view");
+        }});
+        comboBox.getStyleClass().add("combo-box");
+        // Cambiar el tamaño de la ventana
+        String css = new File("src/main/resources/styles.css").toURI().toURL().toExternalForm();
+        Scene scene = new Scene(root, 800, 600); // Cambia el tamaño aquí
+        scene.getStylesheets().add(css);
+
+        primaryStage.setTitle("Music Player");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 }
 
 
