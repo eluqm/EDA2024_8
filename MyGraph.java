e752f3048454e6a9e6eeb7dfdcf36fdfda25af14
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-07-23 02:24:06 -0500

    Creando clase MyGraph, uso de graphos, se coloco el método ddEdge que agrega una arista entre dos nodos. Asegura que ambos nodos estén presentes en el grafo; si no lo están, se añaden primero.

---
+++ MyGraph.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reproductormusic;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
+import java.util.*;
+/**
+ *
+ * @author Usuario
+ */
+public class MyGraph {
+    private Map<String, List<String>> adjList = new HashMap<>();
+
+    // Agregar un nodo al grafo
+    public void addNode(String node) {
+        adjList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    // Agregar una arista entre dos nodos
+    public void addEdge(String from, String to) {
+        adjList.putIfAbsent(from, new ArrayList<>());
+        adjList.putIfAbsent(to, new ArrayList<>());
+        adjList.get(from).add(to);
+    }
+
+    
+}
+
